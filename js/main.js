2d316034e5140c1af66df650f7362b5676d96603
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-07-02 21:21:16 +0200

    Changed site

---
+++ js/main.js
@@ -0,0 +1,99 @@
+data = {
+    'CyberSecLabs - Roast': {
+        'Link': 'https://youtu.be/_VG1g_XRw4U',
+        'Timestamps': '00:00 Introduction\n' +
+            '00:18 HackTricks - Collection of cheatsheets\n' +
+            '00:40 Enumerating ldap with nmap command and finding user with password in the description\n' +
+            '01:20 Running crackmapexec to test found password on found usernames, finding valid credentials\n' +
+            '04:00 Running evil-winrm for crhodes with password\n' +
+            '04:52 Uploading and running SharpHound to the box to ingest data from the domain controller\n' +
+            '06:14 Processing data in BloodHound. We find that there is a path to domain admin from a kerberoastable user roastsvc via GenericWrite\n' +
+            '07:30 Kerberoasting users using GetUserSPNs from impacket\n' +
+            '08:30 Cracking hash from kerberoasted user roastsvc\n' +
+            '09:30 Using cracked password to log in using evil-winrm on roastsvc\n' +
+            '09:50 Exploiting generic write on Domain admin by adding ourselves to the domain admins group\n' +
+            '10:20 Uploading and running mimikatz in order to dump the administrator hash using lsadump\n' +
+            '11:30 Logging in with administrator NTLM hash using evil-winrm'
+    }
+}
+
+function searchword(word) {
+    result = {}
+    for (let video in data) {
+        text = data[video]['Timestamps'];
+        for (let timestamp of text.split('\n')) {
+            if (timestamp.toLowerCase().includes(word.toLowerCase())) {
+                result[video + ' ' + timestamp.substring(0, 5)] = timestamp.substring(6);
+            }
+        }
+    }
+    return result;
+}
+
+
+function showdata(toshow) {
+    let table = document.getElementById('result').lastChild
+    for (let video in toshow) {
+        let tr = document.createElement('tr');
+
+        let td_link = document.createElement('td');
+        let a = document.createElement('a');
+        let time = video.substring(video.length - 5).split(':');
+        a.setAttribute('href', data[video.substring(0, video.length - 6)]['Link'] + '?t=' + (parseInt(time[0]) * 60 + parseInt(time[1])).toString());
+        a.textContent = video.substring(0, video.length - 6);
+        td_link.appendChild(a);
+        tr.appendChild(td_link);
+
+        let td_timestamp = document.createElement('td');
+        td_timestamp.textContent = video.substring(video.length - 5);
+        tr.appendChild(td_timestamp);
+
+        let td_text = document.createElement('td');
+        td_text.textContent = toshow[video];
+        tr.appendChild(td_text);
+
+        table.appendChild(tr);
+    }
+}
+
+function cleardata() {
+    let table = document.getElementById('result').lastChild
+    while (table.firstChild !== table.lastChild) {
+        table.removeChild(table.lastChild);
+    }
+}
+
+function onfocus() {
+    let element = document.getElementById("cover");
+    element.style.width = "80%";
+}
+
+function onfocusout() {
+    let element = document.getElementById("cover");
+    element.style.width = "20%";
+}
+
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        connectParticles: true
+    });
+};
+
+function onclick() {
+    let search = document.getElementById('search')
+    if (search.value.length > 3) {
+       cleardata();
+        showdata(searchword(search.value));
+    }
+}
+
+let element = document.getElementById("cover");
+element.addEventListener("focusin", onfocus);
+element.addEventListener("focusout", onfocusout);
+
+let searchbutton = document.getElementById('searchButton');
+searchbutton.addEventListener('click', onclick);
+document.getElementById('search').addEventListener('input', onclick);
+
+// showdata(data);
